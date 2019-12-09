e3e16d0380e4a9ba7d4342aafe60839c3e58f708
Vasyl.HONcharenko <vasilijgoncarenko4@gmail.com>
2019-12-09 05:32:27 +0200

    ready crud test with faculty web service.

--- src/main/java/ua/nure/honcharenko/webservice/client/FacultyWebServiceClient.java
+++ src/main/java/ua/nure/honcharenko/webservice/client/FacultyWebServiceClient.java
@@ -1,9 +1,12 @@
 package ua.nure.honcharenko.webservice.client;
 
+import ua.nure.honcharenko.entity.faculty.Faculty;
+import ua.nure.honcharenko.entity.faculty.FacultyBranch;
 import ua.nure.honcharenko.webservice.service.FacultyWebService;
 
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
+import java.math.BigInteger;
 import java.net.URL;
 
 public class FacultyWebServiceClient {
@@ -15,9 +18,35 @@ public class FacultyWebServiceClient {
         Service service = Service.create(url, qname);
 
         QName portName = new QName("http://impl.webservice.honcharenkoilchishen.nure.ua/","FacultyWebServiceImplPort");
-        FacultyWebService hello = service.getPort(portName, FacultyWebService.class);
+        FacultyWebService facultyService = service.getPort(portName, FacultyWebService.class);
 
-
-        System.out.println(hello.geFacultyByName("someaq"));
+        Faculty faculty = new Faculty();
+        faculty.setId(2);
+        faculty.setTitle("Custom title");
+        faculty.setShortTitle("Custom short title");
+        faculty.setBudgetPlaceCount(10);
+        faculty.setPlaceCount(15);
+        Faculty.FacultyRoomNumber facultyRoomNumber = new Faculty.FacultyRoomNumber();
+        facultyRoomNumber.setYes("211");
+        faculty.setFacultyRoomNumber(facultyRoomNumber);
+        Faculty.Additional additional = new Faculty.Additional();
+        Faculty.Additional.BacheloerCount bacheloerCount = new Faculty.Additional.BacheloerCount();
+        bacheloerCount.setValue(new BigInteger("25"));
+        bacheloerCount.setYear(2015);
+        faculty.setFacultyBranch(FacultyBranch.PHILOLOGY);
+        additional.setBacheloerCount(bacheloerCount);
+        additional.setEmployedStudents(new BigInteger("50"));
+        faculty.setAdditional(additional);
+        Faculty faculty1 = facultyService.createFaculty(faculty);
+        System.out.println("Faculty with title : " + faculty.getTitle() + " added successfully.");
+        facultyService.deleteFaculty(faculty1.getId());
+        System.out.println("Faculty with id : " + faculty1.getId() + " removed successfully");
+        System.out.println("Try to get faulcty by name 'he' which already is present : " + facultyService.geFacultyByName("he"));
+        System.out.println("Try to get fauclty by name which does not present in xml file: " + facultyService.geFacultyByName("doesNotPresentTitle"));
+        System.out.println("Try to get faculty by id 2 : " + facultyService.getFacultyById(1));
+        Faculty facultyById = facultyService.getFacultyById(1);
+        facultyById.setTitle("First faculty");
+        System.out.println("Try to update faculty with id 2. Change name from 'he' to 'FirstFaculty' :" + facultyService.updateFacultyTitle(facultyById));
+        System.out.println("Get faculty with id 2 after update : " + facultyService.getFacultyById(1));
     }
 }
